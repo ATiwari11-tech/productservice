30aa3b83bdcac79c0dcf78694b2fb98d029c79be
AbhishekT <abhishek.tiwari@fox.com>
2023-09-13 14:22:02 +0530

    Interacting with DB

--- src/main/java/dev/abhishek/productservice/models/Category.java
+++ src/main/java/dev/abhishek/productservice/models/Category.java
@@ -1,7 +1,13 @@
 package dev.abhishek.productservice.models;
 
 import com.fasterxml.jackson.databind.ser.Serializers;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Entity
 public class Category extends BaseModel {
     private String name;
 
