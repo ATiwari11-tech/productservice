bb0d98490aaa7d7af5f3fb812ed592ee684d0744
AbhishekT <abhishek.tiwari@fox.com>
2023-11-13 09:16:13 +0530

    Completed Role Based Authorization

--- src/main/java/dev/abhishek/productservice/security/SpringSecurityConfig.java
+++ src/main/java/dev/abhishek/productservice/security/SpringSecurityConfig.java
@@ -15,10 +15,17 @@ public class SpringSecurityConfig {
             throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
-//                        .requestMatchers("/fakeproducts").hasAuthority("admin")
-                        .anyRequest().authenticated()
+                        .requestMatchers("/fakeproducts").hasAuthority("admin")///fakeproducts endpoint require
+                        // authentication as well as role admin
+                        .anyRequest().authenticated()//other endpoints like /fakeproductst/1
+                        // only require authentication
                 )
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+//                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(
+                        jwtConfigurer -> {
+                            jwtConfigurer.jwtAuthenticationConverter(new CustomJwtAuthenticationConverter());
+                        }
+                ));
 
                 // Form login handles the redirect to the login page from the
                 // authorization server filter chain
